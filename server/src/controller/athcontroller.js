455334141db8be08c719fef4075ca319ae623ddb
Amal <amaljonzn0@gmail.com>
2025-02-19 12:59:00 +0530

    cookie ubdate

--- server/src/controller/athcontroller.js
+++ server/src/controller/athcontroller.js
@@ -1,36 +1,36 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const bcrypt = require("bcrypt");
-const User = require("../model/userModel"); 
+const User = require("../model/userModel");
 
 exports.register = async (req, res) => {
-    
+
     try {
         console.log(req.body)
-        const { username, password, role } =req.body;
+        const { username, password, role } = req.body;
 
         const userExists = await User.findOne({ username });
-        if (userExists) 
+        if (userExists)
             return res.status(400).json({ message: "User already exists" });
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ username, password: hashedPassword, role }) 
+        const newUser = new User({ username, password: hashedPassword, role })
         await newUser.save();
 
-        return res.status(201).json({ message: "User created successfully" , data:newUser}); 
+        return res.status(201).json({ message: "User created successfully", data: newUser });
     } catch (error) {
-       
-        return res.status(500).json({ message: "Server error", error:error.message });
+
+        return res.status(500).json({ message: "Server error", error: error.message });
     }
 };
 
-exports.login = async (req,res)=>{
-    try{
-        const { username, password} =req.body;
+exports.login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
         const user = await User.findOne({ username });
-        if(!user)
+        if (!user)
             return res.status(500).json({ message: "user not found" });
-        const ismatch = await bcrypt.compare(password,user.password)
+        const ismatch = await bcrypt.compare(password, user.password)
         if (!ismatch)
             return res.status(500).json({ message: "password not matching" });
         const token = jwt.sign(
@@ -38,13 +38,17 @@ exports.login = async (req,res)=>{
             process.env.SECRETKEY,
             { expiresIn: "7d" })
 
-            res.cookie('token',token,{httpOnly:true})
-            res.status(200).json({message:"logged in sucess"})
-              
-          
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: true,    // ✅ Ensures cookies are sent only over HTTPS
+            sameSite: "None",
+        })
+        res.status(200).json({ message: "logged in sucess" })
+
+
     }
-    catch(error){
-        return res.status(500).json({ message: "Server error", error:error.message });
+    catch (error) {
+        return res.status(500).json({ message: "Server error", error: error.message });
     }
 }
 exports.logout = (req, res) => {
