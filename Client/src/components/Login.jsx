e844c4d188b213f0c536e3043d7fe91dd19b7d75
Amal <amaljonzn0@gmail.com>
2025-02-19 14:16:41 +0530

    changed redirection in login-1

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -29,11 +29,11 @@ function Login({ setUserRole }) {
       );
 
       setSuccess("Login successful! Redirecting...");
-      //await fetchUser();
 
       setTimeout(() => {
         console.log("Redirecting to Profile...");
         navigate("/");
+        window.location.reload();
       }, 2000);
       
     } catch (err) {
